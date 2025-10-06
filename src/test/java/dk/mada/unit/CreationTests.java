2577d7a80b31644f6234abecf23355b797d64c5e
Jesper Skov <jskov@mada.dk>
2025-10-06 19:33:21 +0200

    Use (mostly) Path instead of File

--- src/test/java/dk/mada/unit/CreationTests.java
+++ src/test/java/dk/mada/unit/CreationTests.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.nio.file.Path;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.io.CleanupMode;
 import org.junit.jupiter.api.io.TempDir;
 
 import dk.mada.mjtar.TarOutputStream;
@@ -15,13 +14,13 @@ import dk.mada.mjtar.TarOutputStream;
  * Tests of constructor variants.
  */
 class CreationTests {
-    @TempDir(cleanup = CleanupMode.NEVER) Path dir;
+    @TempDir Path dir;
 
     /// Tests simple file creation.
     @Test
     void canCreateEmptyArchiveInNewFile() throws IOException {
         Path archive = dir.resolve("a.tar");
-        try (var _ = new TarOutputStream(archive.toFile())) {
+        try (var _ = new TarOutputStream(archive)) {
             // empty
         }
         assertThat(archive)
